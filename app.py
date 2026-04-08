0e0168b2a8251dffd59721a5a4fd6e0fa9c68e87
sivaprasadsde-sp <sivaprasad.sde@gmail.com>
2026-04-08 18:07:53 +0530

    Update app.py with Hurray

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "New version deployed to EC2 🚀"
+    return "Third version deployed to EC2, Hurray 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
