88b4b745f36b7547d05e7f57313e3cacac8e9dcf
sivaprasadsde-sp <sivaprasad.sde@gmail.com>
2026-04-08 18:01:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on EC2 🚀"
+    return "New version deployed to EC2 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
